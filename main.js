6af72a78d92ade889425465232ec81be04166433
darren <darren__c@hotmail.com>
2021-02-04 20:33:42 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log(Math.max(45, 7, 33, 2));
+
+console.log(Math.random() * 5);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  // Math.trunc(Math.random() * (max - min) + 1) + min;
+  // changed to floor because this will work with positive or negative numbers
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
