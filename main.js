0d1669026fd0dc3caf9ef0ee54247e77890e5410
darren <darren__c@hotmail.com>
2021-02-04 23:21:21 +0900

    math and rounding examples

--- main.js
+++ main.js
@@ -11,4 +11,22 @@ const randomInt = (min, max) =>
 
 console.log(randomInt(10, 20));
 
+//Rounding  integers
+
+console.log(Math.round(23.4)); //23
+console.log(Math.round(23.9)); //24
+console.log(Math.round(-23.2)); //-23
+
+console.log(Math.ceil(23.9)); //24
+console.log(Math.ceil(23.3)); //24
+
+console.log(Math.floor(23.3)); //23
+console.log(Math.floor(23.8)); //23
+
+console.log(Math.trunc(23.8)); //23
+console.log(Math.trunc(-23.8)); //-23
+console.log(Math.floor(-23.2)); //-24
+
 // rounding decimals
+console.log((2.7).toFixed(2));
+console.log(+(2.7).toFixed(2));
