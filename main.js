e703941109d570a9dbd966f12dfecc065e3eba28
darren <darren__c@hotmail.com>
2021-02-07 00:17:50 +0900

    setTimer-setInterval

--- main.js
+++ main.js
@@ -38,19 +38,56 @@ console.log(12 % 5); //2
 
 // you can use the odd or even to do functions etc with
 
-const isEven = (n) => n % 2 === 0;
+const isEven = n => n % 2 === 0;
 console.log(isEven(322)); //true
 console.log(isEven(16)); //true
 console.log(isEven(351)); //false
 console.log(isEven(11)); //false
 
 //list experiment
-console.log(document.querySelectorAll(".list li")); //[6]nodelist
+console.log(document.querySelectorAll('.list li')); //[6]nodelist
 
-[...document.querySelectorAll(".list li")].forEach(function (li, i) {
+[...document.querySelectorAll('.list li')].forEach(function (li, i) {
   // console.log(li, i);
   // 0,2,4,6 etc all even
-  if (i % 2 === 0) li.style.backgroundColor = "purple";
+  if (i % 2 === 0) li.style.backgroundColor = 'purple';
   //0,3,6,9 etc all odd
-  if (i % 3 === 0) li.style.backgroundColor = "green";
+  if (i % 3 === 0) li.style.backgroundColor = 'green';
 });
+
+//SetTimeout()
+/*
+setTimeout(
+  (name1, name2) => console.log(`Hey Hi there ${name1} and ${name2}`),
+  8000,
+  "jen",
+  "spaghettibob"
+);
+*/
+
+//can use a array ad the spread operator
+/*
+const employeeNames = ["jen", "spaghettibob", "jengaru"];
+setTimeout(
+  (name1, name2, name3) =>
+    console.log(`Hey Hi there ${name1} and ${name2} and ${name3}`),
+  8000,
+  ...employeeNames // spread operator
+);
+*/
+
+//can clear the timeOut function
+const employeeNames = ['jen', 'spaghettibob', 'jengaru'];
+const employeeListTimer = setTimeout(
+  (name1, name2, name3) =>
+    console.log(`Hey Hi there ${name1} and ${name2} and ${name3}`),
+  3000,
+  ...employeeNames // spread operator
+);
+if (employeeNames.includes('spaghettibob')) clearTimeout(employeeListTimer);
+
+setInterval(function () {
+  const now = new Date();
+
+  // console.log(now.getUTCMinutes());
+}, 3000);
